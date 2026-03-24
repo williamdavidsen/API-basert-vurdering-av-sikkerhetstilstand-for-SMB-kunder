e6582690eaf6b7e4c55713973c517cd719382370
huseyindavut <saskin@protonmail.com>
2026-03-24 10:53:21 +0100

    backup

--- API/obj/Debug/net10.0/SecurityAssessmentAPI.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/SecurityAssessmentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecurityAssessmentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699679b3607af7a4a300afdd19676f6f1a78c958")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28bb55a24ddae1cb9b8e032a35300db0c6b7ee38")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecurityAssessmentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecurityAssessmentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
